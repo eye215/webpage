2c6515594cc6b67ab68aef3c8dc6117c2cf412db
eye215 <54428166+eye215@users.noreply.github.com>
2020-12-18 11:54:04 +0900

    Update js1.js

--- 3/js1.js
+++ 3/js1.js
@@ -1,3 +1,54 @@
+// JavaScript Document
+
+var now=0;
+var imgs=1;
+
+$(document).ready(function(){
+	$(".main_navi>li").mouseover(function(){
+		$(".sub_navi").stop().slideDown();
+		});
+		$(".main_navi>li").mouseleave(function(){
+		$(".sub_navi").stop().slideUp();
+		});
+        
+
+
+		$(".partner").click(function(){
+			$("#partner_pop").css({"display":"block"});
+			});
+			$("#pop>button").click(function(){
+			$("#partner_pop").css({"display":"none"});
+			});
+			
+start();
+
+			});
+
+
+            
+
+
+    $( ".next" ).click(function() {
+        if(!$("#img_slide>li").last().is(":visible")){
+            $("#img_slide>li:visible").hide().next("li").fadeIn("40");
+            
+        }
+       
+        return false;
+    });
+    
+    $( ".prev" ).click(function() {
+        if(!$("#img_slide>li").first().is(":visible")){
+            $("#img_slide>li:visible").hide().prev("li").fadeIn("40");
+        
+        }
+     
+        return false;
+    });
+
+
+
+
 var mainFix = new Swiper('#slide .img_slide', {
     direction: "horizontal",
     autoplay: {
